3623ce5ee0c83028959ec9441f165d5adabae94a
Mariana Granados <89766019+MarianaGranados-09@users.noreply.github.com>
2022-02-09 05:58:47 -0600

    Archivo main.c, archivo .hex y archivo de proteus

---
+++ main.c
@@ -0,0 +1,64 @@
+
+#include <18F4550.h>
+
+
+
+//FUSES
+#FUSES NOWDT  //Fuse No watchdog timer
+//Fuse que hace el "switcheo" entre el cristal externo e interno o detiene al microcontrolador 
+//en caso de que nuestro cristal externo este dañado
+#FUSES NOBROWNOUT 
+#FUSES INTRC //Fuse para cristal interno
+
+#use delay(clock=8M)
+#USE STANDARD_IO(B)
+
+void main()
+{
+
+   
+   while(TRUE)
+   {
+      //LED1
+     output_low(pin_d0);
+     output_high(pin_d1);
+     output_float(pin_d2);
+     delay_ms(500);
+     
+     //LED2
+     output_high(pin_d0);
+     output_low(pin_d1);
+     output_float(pin_d2);
+     delay_ms(500);
+     
+     
+     //LED3
+     output_float(pin_d0);
+     output_low(pin_d1);
+     output_high(pin_d2);
+     delay_ms(500);
+     
+     //LED4
+     output_float(pin_d0);
+     output_high(pin_d1);
+     output_low(pin_d2);
+     delay_ms(500);
+     
+     //LED5
+     output_low(pin_d0);
+     output_float(pin_d1);
+     output_high(pin_d2);
+     delay_ms(500);
+     
+     //LED6
+     output_high(pin_d0);
+     output_float(pin_d1);
+     output_low(pin_d2);
+     delay_ms(500);
+     
+   }
+
+
+
+}
+
